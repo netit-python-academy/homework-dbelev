70697f7e36a59f1fa959c49af28b4d25bd7d65f4
Donislav <donislav.belev@gmail.com>
2021-11-12 23:57:44 +0200

    Lecture-6

---
+++ lecture_6/problem_11.py
@@ -0,0 +1,6 @@
+for a in range(1,50):
+    for b in range(1,50):
+        for c in range(1,50):
+            if c**3 == a**2 + b**2:
+                print(f'{c}^3 = {a}^2 + {b}^^2')
+                
