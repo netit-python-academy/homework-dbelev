295623c3a1c7d7c2f65620b33ce830bd156c1a0a
Donislav <donislav.belev@gmail.com>
2021-11-13 00:56:09 +0200

    Few more Lesson-6

---
+++ lecture_6/problem_14.py
@@ -0,0 +1,14 @@
+import random
+from typing import get_origin
+number = random.randint(1,1000)
+
+guess = 0
+while number != guess:
+    guess = int(input('Guess a number:'))
+    if guess > number:
+        print('Too high')
+    elif guess < number:
+        print('Tool low')
+print('Correct!')
+
+
